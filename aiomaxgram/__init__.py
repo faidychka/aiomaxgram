8c869fda675857ccc3750e164bf21e445d9dadda
fa1dychka <minecraftsobolev@gmail.com>
2025-04-02 15:45:10 +0500

    фикс еюбанееый

--- aiomaxgram/__init__.py
+++ aiomaxgram/__init__.py
@@ -4,10 +4,10 @@ Python Max Bot API Client
 Это библиотека для создания ботов для мессенджера Max
 """
 
-from aioaiomaxgram.bot import Bot
-from aioaiomaxgram.api import Api
-from aioaiomaxgram.context import Context
-from aioaiomaxgram.keyboards import InlineKeyboard
+from aiomaxgram.bot import Bot
+from aiomaxgram.api import Api
+from aiomaxgram.context import Context
+from aiomaxgram.keyboards import InlineKeyboard
 
 __version__ = "0.1.4"
 __all__ = ["Bot", "Api", "Context", "InlineKeyboard"] 
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="aiomaxgram",
-    version="0.1.5.1",
+    version="0.1.5.2",
     packages=find_packages(),
     install_requires=[
         "requests>=2.31.0",
